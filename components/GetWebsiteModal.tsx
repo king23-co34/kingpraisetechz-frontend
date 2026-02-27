160b17208caf202c76133b7d28eceee81c826647
xamuel17 <unachukwu.samuel@gmail.com>
2026-02-27 11:34:55 +0100

    Website modal updated with react-hot-toast

--- components/GetWebsiteModal.tsx
+++ components/GetWebsiteModal.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { X } from "lucide-react";
-import { toast } from "sonner";
+import { toast } from "react-hot-toast";
 
 interface Props {
   open: boolean;
